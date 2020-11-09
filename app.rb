1fbf402704d632df43e21f706e08673e28031bcb
Michael Schats <michael_schats@hotmail.com>
2020-11-09 19:46:37 +0000

    air programming session completed with Josh, created webpage with cat and red border. Managed to create index.erb page with p params

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
